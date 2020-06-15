52ba2f647f08ae20f4adbd5a860a846cc342f740
harshitgindra <harshitgindra@gmail.com>
2020-06-14 20:45:31 -0500

    Refactoring implementation to use Xamarin.Essentials library to share files and info

--- ImageShare/ImageShare.Android/MainActivity.cs
+++ ImageShare/ImageShare.Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -9,7 +8,8 @@ using Android.OS;
 
 namespace ImageShare.Droid
 {
-    [Activity(Label = "ImageShare", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ImageShare", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- ImageShare/ImageShare/MainPage.xaml.cs
+++ ImageShare/ImageShare/MainPage.xaml.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ImageShare
 {
     public partial class MainPage : ContentPage
     {
-        private string ImageLink = "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/02/google-pacman-796x419.jpg";
+        private string ImageLink = "https://www.hexacta.com/wp-content/uploads/2016/03/xamarin.jpg";
+
         public MainPage()
         {
             InitializeComponent();
             SampleImage.Source = ImageLink;
             this.Title = "Share Image";
             SetupNavigationBar();
+        }
+
+        public byte[] GetBytes(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                return webClient.DownloadData(url);
+            }
         }
 
         private void SetupNavigationBar()
@@ -27,12 +39,47 @@ namespace ImageShare
                 {
                     try
                     {
-                        await DependencyService.Get<IShareImage>().Share(ImageLink);
+                        string filename = "temp.jpg";
+                        //***
+                        //*** Get the file name from the link
+                        //***
+                        Uri uri = new Uri(ImageLink);
+                        if (uri.IsFile)
+                        {
+                            filename = System.IO.Path.GetFileName(uri.LocalPath);
+                        }
+
+                        //***
+                        //*** Build a temp path for the file
+                        //***
+                        string tempFileName = Path.Combine(FileSystem.CacheDirectory, filename);
+
+                        //***
+                        //*** Delete the file if exist in the system
+                        //*** 
+                        if (File.Exists(tempFileName))
+                        {
+                            File.Delete(tempFileName);
+                        }
+
+                        //***
+                        //*** Write the file to the directory
+                        //***
+                        File.WriteAllBytes(tempFileName, GetBytes(ImageLink));
+
+                        //***
+                        //*** Share file
+                        //***
+                        await Share.RequestAsync(new ShareFileRequest()
+                        {
+                            File = new ShareFile(tempFileName),
+                            Title = "Sharing image",
+                        });
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex);
-                        await DisplayAlert("Error", "Something went wrong. Please try again later", "OK");
+                        await DisplayAlert("Error", $"Something went wrong. {ex.Message}", "OK");
                     }
                 })
             };
